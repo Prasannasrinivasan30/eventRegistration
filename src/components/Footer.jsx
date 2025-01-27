620a7cec1a3345567e994d82003e508e1c871bdc
Kishore <kishore22705@gmail.com>
2025-01-27 18:01:14 +0530

    Rules and regulations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import {FaWhatsapp, FaInstagram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 const socialLinks = [
-  { href: "https://chat.whatsapp.com/CG1hCNq9TJy8YGxctyqFab", icon: <FaWhatsapp className="size-5" /> },
+  { href: "https://chat.whatsapp.com/CG1hCNq9TJy8YGxctyqFab", icon: <FaWhatsapp className="size-6" /> },
   // { href: "https://youtube.com", icon: <FaYoutube className="size-5"/> },
-  { href: "mailto:honeken@rmkec.ac.in", icon: <SiGmail className="size-5"/> },
-  { href: "https://Instagram.com", icon: <FaInstagram className="size-5"/> },
+  { href: "mailto:honeken@rmkec.ac.in", icon: <SiGmail className="size-6"/> },
+  { href: "https://Instagram.com", icon: <FaInstagram className="size-6"/> },
 ];
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-[#5542ff] py-4 text-black">
+    <footer className="w-screen bg-[#5542ff] py-6 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-medium md:text-left">
           © R.M.K. 2025. All rights reserved
